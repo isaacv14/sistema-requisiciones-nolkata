8acbab7b4eea0065876a2d6a8015f309607565db
Josue Ortiz <rjosueoo25@gmail.com>
2026-05-28 16:24:56 -0500

    feat Persona

---
+++ Clases/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolkataInc.Clases
+{
+    public abstract class Persona
+    {
+        public long idPersona { get; }
+        private string nombrePersona { get; }
+        private string emailPersona { get; }
+
+        public Persona (long idPersona, string nombrePersona, string emailPersona)
+        {
+            this.idPersona = idPersona;
+            this.nombrePersona = nombrePersona;
+            this.emailPersona = emailPersona;
+
+        }
+
+    }
+}
